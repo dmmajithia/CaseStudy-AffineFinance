68027741b7c3ee707a6deab3849ad48ea3620a51
Dhawal Majithia <dmmajithia@ucdavis.edu>
2019-01-31 04:11:08 +0000

    installed mongodb npm

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ var port = 8080;
 app.use("/", (req, res) => {
  res.sendFile(__dirname + "/index.html");
 });
+
+app.get("/name", (req, res) => {
+ var name = req.query.name;
+ var mongoose = require("mongoose");
+ mongoose.Promise = global.Promise;
+ mongoose.connect("mongodb://dhawal:vd1vd2vd3@localhost:27017/case_study");
+ 
+});
  
 app.listen(port, () => {
  console.log("Server listening on port " + port);
